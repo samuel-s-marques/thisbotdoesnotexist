ce0dff1ec4ba6c0a3f00c87c8fe24f17d7ff3e32
Samuel Marques <samuel.s.marques@protonmail.com>
2023-11-01 09:45:21 -0300

    fix elevenlabs implementation

--- src/tts/elevenlabs_api.ts
+++ src/tts/elevenlabs_api.ts
@@ -1,8 +1,9 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import fs from "fs";
 import { v4 as uuidv4 } from "uuid";
 import { processFile } from "../util/util.js";
 import dotenv from "dotenv";
+import { VoiceModel } from "./voice_model.js";
 
 export class ElevenLabsApi {
   private baseUrl: string;
@@ -15,34 +16,52 @@ export class ElevenLabsApi {
     this.apiKey = process.env.ELEVENLABS_API_KEY;
   }
 
-  async getTextToSpeech(voiceId: string, text: string): Promise<string> {
-    const url = `${this.baseUrl}v1/text-to-speech/${voiceId}/stream`;
+  async getTextToSpeech(voice: VoiceModel, text: string): Promise<string> {
+    const url = `${this.baseUrl}/v1/text-to-speech/${voice.id}/stream`;
 
     try {
       if (!fs.existsSync("output")) {
         fs.mkdirSync("output");
       }
 
-      const response = await axios.post(
-        url,
-        {
+      console.log(`🤖 [server]: Making Text-to-Speech request to ElevenLabs...`);
+
+      const streamOptions: AxiosRequestConfig = {
+        method: "POST",
+        url: url,
+        data: {
           text: text,
           model_id: "eleven_multilingual_v2",
           voice_settings: { stability: 0.5, similarity_boost: 0.5 },
         },
-        {
-          headers: {
-            accept: "audio/mpeg",
-            "Content-Type": "application/json",
-            "xi-api-key": this.apiKey,
-          },
-          responseType: "stream",
+        headers: {
+          accept: "audio/mpeg",
+          "Content-Type": "application/json",
+          "xi-api-key": this.apiKey,
         },
-      );
-      const uuid = uuidv4();
-      response.data.pipe(fs.createWriteStream(`output/audios/${uuid}.mp3`));
+        responseType: "stream",
+      };
 
-      return `output/audios/${uuid}.mp3`;
+      const response = await axios(streamOptions);
+
+      const uuid = uuidv4();
+      const writer = fs.createWriteStream(`output/audios/${uuid}.mp3`);
+
+      return new Promise((resolve, reject) => {
+        writer.on("finish", () => {
+          console.log(`🤖 [server]: ElevenLabs' audio ${uuid}.mp3 saved.`);
+          resolve(`output/audios/${uuid}.mp3`);
+        });
+
+        writer.on("error", (error) => {
+          console.error(
+            `💀 [server]: Error saving audio ${uuid}.mp3: ${error}`,
+          );
+          reject(error);
+        });
+
+        response.data.pipe(writer);
+      });
     } catch (error) {
       console.error(
         `💀 [server]: Error making Text-to-Speech request to ElevenLabs: ${error}`,
@@ -67,7 +86,7 @@ export class ElevenLabsApi {
     }
   }
 
-  getRandomVoiceId(gender: string) {
+  getRandomVoice(gender: string): VoiceModel {
     const voices = processFile("elevenlabs_voices").voices.filter(
       (voice: { labels: { gender: string } }) => voice.labels.gender === gender,
     );
@@ -77,6 +96,15 @@ export class ElevenLabsApi {
     }
 
     const randomVoice = voices[Math.floor(Math.random() * voices.length)];
-    return randomVoice.voice_id;
+    const voiceModel: VoiceModel = new VoiceModel(
+      randomVoice.name,
+      randomVoice.voice_id,
+      randomVoice.labels.gender,
+      null,
+      null,
+      null,
+    );
+
+    return voiceModel;
   }
 }
